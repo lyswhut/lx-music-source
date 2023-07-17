7cc32880a3f52c1dd3b8a03247a309af8b914c58
lyswhut <lyswhut@qq.com>
2023-07-17 20:55:41 +0800

    update version

--- dist/lx-music-source.js
+++ dist/lx-music-source.js
@@ -1,7 +1,7 @@
 /*!
  * @name A lx-music source
- * @description v1.0.3
- * @version v1.0.3
+ * @description v1.0.4
+ * @version v1.0.4
  */
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
@@ -50,7 +50,8 @@ const qualitys = {
   musicUrl({ songmid }, quality) {
     quality = qualitys[quality]
 
-    const target_url = `http://www.kuwo.cn/api/v1/www/music/playUrl?mid=${songmid}&type=convert_url3&br=${quality}`
+    const target_url = `http://www.kuwo.cn/api/v1/www/music/playUrl?mid=${songmid}&type=music&br=${quality}`
+    // const target_url = `http://www.kuwo.cn/api/v1/www/music/playUrl?mid=${songmid}&type=convert_url3&br=${quality}`
     /* const target_url = 'https://www.kuwo.cn/url?'
       + `format=mp3&rid=${song_id}&response=url&type=convert_url3&br=128kmp3&from=web`;
     https://m.kuwo.cn/newh5app/api/mobile/v1/music/src/${song_id} */
@@ -62,6 +63,8 @@ const qualitys = {
         headers: {
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0',
           Referer: 'http://kuwo.cn/',
+          Secret: '6c3e1759abe6bd58f56bb713f6aee0bb738189eae7837be83636389b96fd4d7104c13520',
+          Cookie: 'Hm_Iuvt_cdb524f42f0ce19b169b8072123a4727=2bm5QbPQKPZSRHyFN4pbZnGcNJ4J2DZJ',
         },
       }, (err, resp) => {
         console.log(resp.body)
