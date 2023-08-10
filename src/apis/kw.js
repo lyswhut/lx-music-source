139519973204c8d49424d9421192f0ab6246b7bd
lyswhut <lyswhut@qq.com>
2023-08-10 13:55:21 +0800

    fix kw api

--- src/apis/kw.js
+++ src/apis/kw.js
@@ -8,6 +8,91 @@ const qualitys = {
 }
 
 
+let token = ''
+let cookie = ''
+let key = ''
+
+function encrypt(str, pwd) {
+  if (pwd == null || pwd.length <= 0) {
+    console.log('Please enter a password with which to encrypt the message.')
+    return null
+  }
+  let prand = ''
+  for (let i = 0; i < pwd.length; i++) {
+    prand += pwd.charCodeAt(i).toString()
+  }
+  let sPos = Math.floor(prand.length / 5)
+  let mult = parseInt(prand.charAt(sPos) + prand.charAt(sPos * 2) + prand.charAt(sPos * 3) + prand.charAt(sPos * 4) + prand.charAt(sPos * 5))
+  let incr = Math.ceil(pwd.length / 2)
+  let modu = Math.pow(2, 31) - 1
+  if (mult < 2) {
+    console.log('Algorithm cannot find a suitable hash. Please choose a different password. \nPossible considerations are to choose a more complex or longer password.')
+    return null
+  }
+  let salt = Math.round(Math.random() * 1000000000) % 100000000
+  prand += salt
+  while (prand.length > 10) {
+    prand = (parseInt(prand.substring(0, 10)) + parseInt(prand.substring(10, prand.length))).toString()
+  }
+  prand = (mult * prand + incr) % modu
+  let enc_chr = ''
+  let enc_str = ''
+  for (let i = 0; i < str.length; i++) {
+    enc_chr = parseInt(str.charCodeAt(i) ^ Math.floor((prand / modu) * 255))
+    if (enc_chr < 16) {
+      enc_str += '0' + enc_chr.toString(16)
+    } else enc_str += enc_chr.toString(16)
+    prand = (mult * prand + incr) % modu
+  }
+  salt = salt.toString(16)
+  while (salt.length < 8)salt = '0' + salt
+  enc_str += salt
+  return enc_str
+}
+const createToken = (cookieToken, currentKey) => {
+  if (currentKey && key != currentKey) key = currentKey
+  return encrypt(cookieToken, key)
+}
+const parseCookieToken = (cookies) => {
+  if (!cookies) return ''
+  let cookieToken = cookies.find(str => str.startsWith('Hm_Iuvt_'))
+  if (!cookieToken) return ''
+  cookieToken = cookieToken.split(';')[0]
+  cookie = cookieToken
+  cookieToken = cookieToken.split('=')[1]
+  return cookieToken
+}
+const getToken = () => new Promise((resolve, reject) => {
+  let defaultKey = 'Hm_Iuvt_cdb524f42f0ce19b169a8071123a4700'
+  request('http://www.kuwo.cn/', {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0',
+      Referer: 'http://www.kuwo.cn/',
+    },
+  }, function(error, response) {
+    if (error) return reject(new Error('failed'))
+    const token = parseCookieToken(response.headers['set-cookie'])
+    if (!token) return reject(new Error('Invalid cookie'))
+    const result = response.body.match(/app\.\w+\.js/)
+    if (result) {
+      request(`https://h5static.kuwo.cn/www/kw-www/${result[0]}`, {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0',
+          Referer: 'http://www.kuwo.cn/',
+        },
+      }, function(error, response) {
+        if (error) return resolve(createToken(token, defaultKey))
+        const result = response.body.match(/Hm_Iuvt_(\w+)/)
+        if (result) {
+          resolve(createToken(token, result[0]))
+        } else resolve(createToken(token, defaultKey))
+      })
+    } else {
+      resolve(createToken(token, defaultKey))
+    }
+  })
+})
+
 export default {
   info: {
     name: '酷我音乐',
@@ -16,7 +101,7 @@ export default {
     qualitys: ['128k', '320k'],
   },
 
-  musicUrl({ songmid }, quality) {
+  async musicUrl({ songmid }, quality) {
     quality = qualitys[quality]
 
     const target_url = `http://www.kuwo.cn/api/v1/www/music/playUrl?mid=${songmid}&type=music&br=${quality}`
@@ -25,6 +110,8 @@ export default {
       + `format=mp3&rid=${song_id}&response=url&type=convert_url3&br=128kmp3&from=web`;
     https://m.kuwo.cn/newh5app/api/mobile/v1/music/src/${song_id} */
 
+    if (!token) token = await getToken()
+
     return new Promise((resolve, reject) => {
       // console.log(songmid, quality)
       request(target_url, {
@@ -32,8 +119,8 @@ export default {
         headers: {
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0',
           Referer: 'http://kuwo.cn/',
-          Secret: '6c3e1759abe6bd58f56bb713f6aee0bb738189eae7837be83636389b96fd4d7104c13520',
-          Cookie: 'Hm_Iuvt_cdb524f42f0ce19b169b8072123a4727=2bm5QbPQKPZSRHyFN4pbZnGcNJ4J2DZJ',
+          Secret: token,
+          cookie,
         },
       }, (err, resp) => {
         console.log(resp.body)
